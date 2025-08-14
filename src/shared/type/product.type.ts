1dd8e5a5d40b4c11eaa97dc62dcdbe1e44b19b80
MakhmudNunu <mahmud.nunu001@gmail.com>
2025-08-14 23:10:17 +0600

    some addons before starting projects

---
+++ src/shared/type/product.type.ts
@@ -0,0 +1,6 @@
+export type TProduct = {
+    name: string
+    price: number
+}
+
+// Тоже для примера
---
+++ src/widget/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            Здесь Footer
+        </footer>
+    )
+}
